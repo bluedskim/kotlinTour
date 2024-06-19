1c5ac05e9291b1293a414d0e649a7d9adee080b5
Dennis <bluedskim@gmail.com>
2024-06-20 00:43:39 +0900

    add collections exercise 1

---
+++ src/07_Collections exercise1.kt
@@ -0,0 +1,6 @@
+fun main() {
+//    You have a list of “green” numbers and a list of “red” numbers. Complete the code to print how many numbers there are in total.
+    val greenNumbers = listOf(1,4,23)
+    val redNumbers = listOf(17,2)
+    println("total count: ${greenNumbers.count() + redNumbers.count()}");
+}
