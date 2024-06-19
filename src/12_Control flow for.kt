e18bb1e06f7b364ac4e7e99125f003f6a5a010d1
Dennis <bluedskim@gmail.com>
2024-06-20 00:44:14 +0900

    add control flow for examples

---
+++ src/12_Control flow for.kt
@@ -0,0 +1,10 @@
+fun main() {
+    for ( number in 1..5) {
+        print(number)
+    }
+
+    val cakes = listOf("carrot", "cheese", "chocolate")
+    for(cake in cakes) {
+        println("Yummy, it's a $cake cake!")
+    }
+}
