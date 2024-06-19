539512ad862d7c637544b09a10cc20c894c2c101
Dennis <bluedskim@gmail.com>
2024-06-20 00:44:19 +0900

    add control flow while examples

---
+++ src/13_Control flow while.kt
@@ -0,0 +1,20 @@
+fun main() {
+    var cakesEaten = 0
+    while(cakesEaten < 3) {
+        println("Eat a cake")
+        cakesEaten++
+    }
+
+    cakesEaten = 0
+    var cakesBaked = 0
+    while(cakesEaten < 3) {
+        println("Eat a cake")
+        cakesEaten++
+    }
+
+    do {
+        println("Bake a cake")
+        cakesBaked++
+    } while (cakesBaked < cakesEaten)
+
+}
