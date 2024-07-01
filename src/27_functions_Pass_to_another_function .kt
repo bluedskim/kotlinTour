0e39d7cd688fa91fbe004024d828be4081ab0cd5
Dennis <bluedskim@gmail.com>
2024-07-02 00:28:22 +0900

    add function pass to another functions example

---
+++ src/27_functions_Pass_to_another_function .kt
@@ -0,0 +1,7 @@
+fun main() {
+    val numbers = listOf(1,-2,3,-4,5,-6)
+    val positives = numbers.filter { x -> x > 0 }
+    val negatives = numbers.filter { x -> x < 0 }
+    println(positives)
+    println(negatives)
+}
